de0458dcad6a4a259f69d319517323167469ea27
Bartosz Hejman <bartoszhejman@gmail.com>
2020-04-10 20:29:15 +0200

    Update docker-compose

--- utils/time/time.go
+++ utils/time/time.go
@@ -1,22 +1,22 @@
 package time
 
-import timePckg "time"
+import "time"
 
-func BeginningOfHour(t timePckg.Time) timePckg.Time {
+func BeginningOfHour(t time.Time) time.Time {
 	y, m, d := t.Date()
-	return timePckg.Date(y, m, d, t.Hour(), 0, 0, 0, t.Location())
+	return time.Date(y, m, d, t.Hour(), 0, 0, 0, t.Location())
 }
 
-func BeginningOfDay(t timePckg.Time) timePckg.Time {
+func BeginningOfDay(t time.Time) time.Time {
 	y, m, d := t.Date()
-	return timePckg.Date(y, m, d, 0, 0, 0, 0, t.Location())
+	return time.Date(y, m, d, 0, 0, 0, 0, t.Location())
 }
 
-func EndOfHour(t timePckg.Time) timePckg.Time {
-	return BeginningOfHour(t).Add(timePckg.Hour - timePckg.Nanosecond)
+func EndOfHour(t time.Time) time.Time {
+	return BeginningOfHour(t).Add(time.Hour - time.Nanosecond)
 }
 
-func EndOfDay(t timePckg.Time) timePckg.Time {
+func EndOfDay(t time.Time) time.Time {
 	y, m, d := t.Date()
-	return timePckg.Date(y, m, d, 23, 59, 59, int(timePckg.Second - timePckg.Nanosecond), t.Location())
+	return time.Date(y, m, d, 23, 59, 59, int(time.Second - time.Nanosecond), t.Location())
 }
