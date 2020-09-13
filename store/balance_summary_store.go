3b502c22ba45ac1b5d0a1a6c44a570f79da59228
Jessica Paczuski <jessica@figment.network>
2020-09-13 16:25:35 -0400

    Implement /balance/:address route

--- store/balance_summary_store.go
+++ store/balance_summary_store.go
@@ -17,6 +17,7 @@ type BalanceSummaryStore interface {
 	BaseStore
 
 	Find(*model.BalanceSummary) (*model.BalanceSummary, error)
+	GetDailySummaries(address, start, end string) ([]model.BalanceSummary, error)
 	FindActivityPeriods(types.SummaryInterval, int64) ([]ActivityPeriodRow, error)
 }
 
@@ -55,6 +56,41 @@ func (s *balanceSummaryStore) FindActivityPeriods(interval types.SummaryInterval
 	var res []ActivityPeriodRow
 	for rows.Next() {
 		var row ActivityPeriodRow
+		if err := s.db.ScanRows(rows, &row); err != nil {
+			return nil, err
+		}
+		res = append(res, row)
+	}
+	return res, nil
+}
+
+// GetDailySummaries Gets daily summary of balance events
+func (s *balanceSummaryStore) GetDailySummaries(address, start, end string) ([]model.BalanceSummary, error) {
+	t := metrics.NewTimer(databaseQueryDuration.WithLabels("BalanceSummaryStore_GetDailySummaries"))
+	defer t.ObserveDuration()
+
+	tx := s.db.
+		Table(model.BalanceSummary{}.TableName()).
+		Select("*").
+		Where("address = ? AND time_interval='day'", address).
+		Order("time_bucket")
+
+	if end != "" {
+		tx = tx.Where("time_bucket <= ?", end)
+	}
+	if start != "" {
+		tx = tx.Where("time_bucket >= ?", start)
+	}
+
+	rows, err := tx.Rows()
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var res []model.BalanceSummary
+	for rows.Next() {
+		var row model.BalanceSummary
 		if err := s.db.ScanRows(rows, &row); err != nil {
 			return nil, err
 		}
---
+++ usecase/balance/get_for_address_http_handler.go
@@ -0,0 +1,68 @@
+package balance
+
+import (
+	"net/http"
+
+	"github.com/figment-networks/oasishub-indexer/client"
+	"github.com/figment-networks/oasishub-indexer/store"
+	"github.com/figment-networks/oasishub-indexer/types"
+	"github.com/figment-networks/oasishub-indexer/utils/logger"
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+)
+
+var (
+	_ types.HttpHandler = (*getForAddressHttpHandler)(nil)
+)
+
+type getForAddressHttpHandler struct {
+	db     *store.Store
+	client *client.Client
+
+	useCase *getForAddressUseCase
+}
+
+func NewGetForAddressHttpHandler(db *store.Store, c *client.Client) *getForAddressHttpHandler {
+	return &getForAddressHttpHandler{
+		db:     db,
+		client: c,
+	}
+}
+
+type GetForAddressRequest struct {
+	Address string `uri:"address" binding:"required"`
+	Start   string `form:"start" binding:"-"`
+	End     string `form:"end" binding:"-"`
+}
+
+func (h *getForAddressHttpHandler) Handle(c *gin.Context) {
+	var req GetForAddressRequest
+	if err := c.ShouldBindUri(&req); err != nil {
+		logger.Error(err)
+		err := errors.New("invalid address")
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+	if err := c.ShouldBindQuery(&req); err != nil {
+		logger.Error(err)
+		err := errors.New("invalid kind or/and after")
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	resp, err := h.getUseCase().Execute(req.Address, req.Start, req.End)
+	if err != nil {
+		logger.Error(err)
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, resp)
+}
+
+func (h *getForAddressHttpHandler) getUseCase() *getForAddressUseCase {
+	if h.useCase == nil {
+		h.useCase = NewGetForAddressUseCase(h.db)
+	}
+	return h.useCase
+}
