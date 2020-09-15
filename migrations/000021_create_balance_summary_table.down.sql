1a69143d31486fffc39b8c356707af8a1f2c6efe
Jessica Paczuski <jessica@figment.network>
2020-09-15 11:00:21 -0400

    Fix migration file

--- migrations/000021_create_balance_summary_table.down.sql
+++ migrations/000021_create_balance_summary_table.down.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS balance_events;
+DROP TABLE IF EXISTS balance_summary;
 
 DROP INDEX IF EXISTS idx_balance_summary_time;
 DROP INDEX IF EXISTS idx_balance_summary_index_version;
